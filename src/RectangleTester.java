dad4f037682c6ef1f05ea502a1212fe4aee3bd4a
delcarlswlhs <delcarls@wlhs.wlwv.k12.or.us>
2023-02-07 09:30:46 -0800

    worked on rectangle

--- src/RectangleTester.java
+++ src/RectangleTester.java
@@ -1,9 +1,14 @@
 public class RectangleTester {
     public static void main(String[] args) {
         //test your rectangle here
-        Rectangle rect1 = new Rectangle(7.45676, 394.488543);
-        double expectedPerimeter = 2*7.45676 + 2*394.488543;
-        System.out.println(expectedPerimeter);
+        Rectangle rect1 = new Rectangle(5, 10);
+        System.out.println(rect1);
+        rect1.setWidth(4);
+        rect1.setHeight(15);
+        System.out.println(rect1);
+        System.out.println(rect1.calculateArea());
         System.out.println(rect1.calculatePerimeter());
+        rect1.resetRectangle(rect1);
+        System.out.println(rect1);
     }
 }
